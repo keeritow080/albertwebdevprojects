ec3ad5e29286660c948a437a7cf6d1b919bed69b
keeritow080 <104501063+keeritow080@users.noreply.github.com>
2025-09-26 21:31:07 +0800

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,10 +1,27 @@
 const  carousel = document.getElementById('carousel');
 const prev = document.querySelector('prev');
-const next = document.querySelector('next;');
+const	 next = document.querySelector('next;');
 
 let index= 0;
 const total Cards = carousel.children.length;
 function getCardWidth(){
 	const card = carousel.children[0];
 	const style = getComputedStlye(card);
-	
+	return card.offsetwidth + parseInt(style.margin.left)+
+parseInt (style.marginRight);
+}
+function update+carousel(){
+	const CardWidth = getCardWidth();
+	carousel.style.transform = translateX(${-index* cardwidth}px);
+}
+next.addEventListener('click'()=>{
+index=(index + 1)%totalcards;
+updatecarousel();)
+});
+prev.addEventListener('click'()=>{
+index=(index - 1 + totalcards) %totalcards;
+updatecarousel();
+});
+windows.addEventListener('resize',updatecarousel);
+windows.addEventListener('load',updatecarousel);
+

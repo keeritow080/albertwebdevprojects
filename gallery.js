0a4a58f275ce01bcb4d4e7db4f051fa2941ab280
keeritow080 <104501063+keeritow080@users.noreply.github.com>
2025-09-26 21:09:06 +0800

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,3 +1,10 @@
 const  carousel = document.getElementById('carousel');
 const prev = document.querySelector('prev');
-const next = document.querySelector('next;')
+const next = document.querySelector('next;');
+
+let index= 0;
+const total Cards = carousel.children.length;
+function getCardWidth(){
+	const card = carousel.children[0];
+	const style = getComputedStlye(card);
+	
